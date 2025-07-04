422219bc6daf83a86f00fed177c6d3bdf554e1ee
charlotte-ht <135715018+charlotte-ht@users.noreply.github.com>
2025-07-04 15:38:51 +0200

    Update global_polyA.sh

--- post-processing/polyA/global_polyA.sh
+++ post-processing/polyA/global_polyA.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
-#the purpose of this file is to extract the polyA tags from the aligned bam files and to store them in a tab separated tablr, where column1=unique_read_id and column2=polyAlength (as determined by dorado during the basecalling step)
+#the purpose of this file is to extract the polyA tags from the aligned bam files 
+#and to store them in a tab separated table, where column1=unique_read_id and column2=polyAlength (as determined by dorado during the basecalling step)
 ml samtools
 
 function extract_all {
 	small=$( basename $1 | sed 's/GRCh38//;s/basecall//;s/.bam//' | tr -s "." | sed -e 's/0.7.2./0.7.2/' )
 	echo $small
+ 	#the format between RNA002 and RNA004 is different, thus we allocate chemistry via the filename and process accordingly
 	case "$1" in
 		*"002"*)
 		samtools view -@8 -F 4  $1 | grep -P "pt:i:[0-9]*" | awk '{print $1"\t"$NF}' | sort -u | sed "s/pt:i:/$small\t/" > $small.aligned.global.poly_a_tags.tsv ;;
@@ -15,7 +17,7 @@ function extract_all {
 			 }
 export -f extract_all
 
-input_all=$( ls ../*bam | grep -P -v "HEK|_E" )
+input_all=$( ls ../*bam )
 parallel -v extract_all ::: $input_all
 
 
