1364ea550011a47c78c84620065aa0207c33d382
charlotte-ht <135715018+charlotte-ht@users.noreply.github.com>
2025-07-04 13:44:24 +0200

    Update subset_to_chr20.sh

--- post-processing/chr20_legacy_callers/subset_to_chr20.sh
+++ post-processing/chr20_legacy_callers/subset_to_chr20.sh
@@ -25,4 +25,17 @@ mkdir -p chr20_rna002_lemke_a_fast5
 #convert to fast5
 #because legacy callers take fast5 as input po5 needs to be converted to fast5 format
 pod5 convert to_fast5 ./chr20_rna002_lemke_a_pod5/chr20_rna002_lemke_a.filtered.pod5 --output ./chr20_rna002_lemke_a_fast5
+#repeat for the other samples
+##mkdir -p ./chr20_rna002_lemke_b_pod5/
+##samtools view RNA002_B_basecall.0.7.2.GRCh38.chr20.bam | cut -f1 | sort -u >read_idsB.txt
+##pod5 filter /mnt/ssd_share_01/new_folder/RESEARCH/20230714_Lemke_DRS_RNA002_B_Run2/Sample_B/20230714_1506_2B_PAG68254_d732e056/pod5/*.pod5 --output ./chr20_rna002_lemke_b_pod5/chr20_rna002_lemke_b.filtered.pod5 --ids read_idsB.txt --missing-ok
+##mkdir -p chr20_rna002_lemke_b_fast5
+##pod5 convert to_fast5 ./chr20_rna002_lemke_b_pod5/chr20_rna002_lemke_b.filtered.pod5 --output ./chr20_rna002_lemke_b_fast5
+
+##echo "start uhrr"
+##mkdir -p ./chr20_rna002_uhrr_pod5/
+##samtools view RNA002_UHRR_basecall.0.7.2.GRCh38.chr20.bam | cut -f1 | sort -u >read_idsUHRR.txt
+##pod5 filter /mnt/ssd_share_01/new_folder/RESEARCH/20230714_UHRR_R9_DRS/UHRR_R9_DRS/20230714_1508_3A_PAG68321_9f572de7/pod5/*.pod5 --output ./chr20_rna002_uhrr_pod5/chr20_rna002_uhrr_pod5.filtered.pod5 --ids read_idsUHRR.txt --missing-ok
+##mkdir -p ./chr20_rna002_uhrr_fast5
+##pod5 convert to_fast5 ./chr20_rna002_uhrr_pod5/chr20_rna002_uhrr_pod5.filtered.pod5 --output ./chr20_rna002_uhrr_fast5
 
